460bb24252a06b2410a7d6a9ead09435e5b59183
Tam Phan <48336678+tam135@users.noreply.github.com>
2019-10-31 01:57:23 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,8 @@
-/*
-	Hyperspace by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
-
 	var	$window = $(window),
 		$body = $('body'),
 		$sidebar = $('#sidebar');
 
-	// Breakpoints.
 		breakpoints({
 			xlarge:   [ '1281px',  '1680px' ],
 			large:    [ '981px',   '1280px' ],
@@ -18,21 +10,15 @@
 			small:    [ '481px',   '736px'  ],
 			xsmall:   [ null,      '480px'  ]
 		});
-
-	// Hack: Enable IE flexbox workarounds.
 		if (browser.name == 'ie')
 			$body.addClass('is-ie');
 
-	// Play initial animations on page load.
 		$window.on('load', function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
 			}, 100);
 		});
 
-	// Forms.
-
-		// Hack: Activate non-input submits.
 			$('form').on('click', '.submit', function(event) {
 
 				// Stop propagation, default.
@@ -44,7 +30,6 @@
 
 			});
 
-	// Sidebar.
 		if ($sidebar.length > 0) {
 
 			var $sidebar_a = $sidebar.find('a');
@@ -55,14 +40,13 @@
 
 					var $this = $(this);
 
-					// External link? Bail.
 						if ($this.attr('href').charAt(0) != '#')
 							return;
 
-					// Deactivate all links.
+
 						$sidebar_a.removeClass('active');
 
-					// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+					
 						$this
 							.addClass('active')
 							.addClass('active-locked');
@@ -74,11 +58,10 @@
 						id = $this.attr('href'),
 						$section = $(id);
 
-					// No section for this link? Bail.
 						if ($section.length < 1)
 							return;
 
-					// Scrollex.
+
 						$section.scrollex({
 							mode: 'middle',
 							top: '-20vh',
